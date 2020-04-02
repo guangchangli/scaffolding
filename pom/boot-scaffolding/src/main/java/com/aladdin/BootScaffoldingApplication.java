26de216717eace894e3a875a4d9da33aa09e655f
guangchangli <guangchangde@gmail.com>
2020-04-02 22:56:40 +0800

    add 'pageHelper'

--- pom/boot-scaffolding/src/main/java/com/aladdin/BootScaffoldingApplication.java
+++ pom/boot-scaffolding/src/main/java/com/aladdin/BootScaffoldingApplication.java
@@ -1,10 +1,16 @@
 package com.aladdin;
 
+import com.github.pagehelper.PageHelper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
 
 @SpringBootApplication
+@MapperScan(basePackages = {"com.aladdin.dao"})
 public class BootScaffoldingApplication {
 
     public static void main(String[] args) {
@@ -12,5 +18,19 @@ public class BootScaffoldingApplication {
         springApplication.setBannerMode(Banner.Mode.OFF);
         springApplication.run(args);
     }
+
+    @Bean
+    public PageHelper getPageHelper(){
+        PageHelper pageHelper=new PageHelper();
+        Properties properties=new Properties();
+        properties.setProperty("helperDialect","mysql");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("supportMethodsArguments","true");
+        properties.setProperty("params","count=countSql");
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+
+
 
 }
